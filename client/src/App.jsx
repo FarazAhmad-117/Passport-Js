5f9bbeef4745ec283de5132b6d99df9a8cfc7028
FarazAhmad-117 <farazahmad31048@gmail.com>
2024-09-11 16:28:37 +0500

    initializing client size

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="text-4xl">
+      <h1 className="text-3xl">Hello world!</h1>
+    </div>
+  );
+};
+
+export default App;
