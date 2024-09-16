7571fe69f694b7564373596bf6d6cdc7e22bb6be
FarazAhmad-117 <farazahmad31048@gmail.com>
2024-09-16 21:48:58 +0500

    som efixes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -8,15 +8,19 @@ const passport = require("passport")
 router.get("/login/success", (req, res) => {   // When we get logged in by google oAuth then checking this route
     console.log(req.user)
     if (req.user) {
-        res.status(200).json({ success: true, message: "successful", user: req.user })
+        return res.status(200).json({ success: true, message: "successful", user: req.user })
     }
     return res.status(403).json({ success: false, message: "unsuccessful", user: null })
 });
 
 
 router.get("/logout", (req, res) => {
-    req.logout();
-    res.redirect(process.env.CLIENT_URL);
+    req.logout(function (err) {
+        if (err) {
+            return next(err);
+        }
+        res.redirect(process.env.CLIENT_URL);
+    });
 });
 
 
